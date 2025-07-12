cfd3f16f99edf6e60677e3aa355a19b0a7dbcba9
tgede46 <gedeonkpara@gmail.com>
2025-07-12 18:40:04 +0000

    composant avec anguylar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,33 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template:`
+ <h1>{{title}}</h1>
+<p>age : {{age +10 }}</p>
+<input type="text  "/>
+
+val : {{counter}}
+<button (click)="incre() ">Incrementation</button>
+<button (click)="decre()">decrementation</button>
+<button (click)="init()">initalse</button>
+  `,
+  // template: `toto`,
+  // templateUrl: './app.component.html',
+  // styles:'h1 { color: green; }',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'employe';
+  title = 'my first angular app';
+  age = 20;
+
+  counter= 0;
+  incre(){
+    this.counter++;
+  }
+  decre(){
+    this.counter--;
+  }
+  init(){
+this.counter=0;
+  }
 }
